88ee4da87be92bcfb8e59915e63ed2d78ba17fe4
Artem Dobrovinskiy <dobrowins@users.noreply.github.com>
2016-11-06 22:25:01 +0300

    done

--- app/src/main/java/com/example/anon/phonedetecttest/MainActivity.java
+++ app/src/main/java/com/example/anon/phonedetecttest/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /** setting variables to work with */
+        // setting variables to work with
         final String productSt = Build.PRODUCT;
         final String manufacturerSt = Build.MANUFACTURER;
         final String deviceSt = Build.DEVICE;
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         final TextView brand = (TextView)findViewById(R.id.brand);
         final TextView model = (TextView)findViewById(R.id.model);
 
-        /** MEGA-DETECTOR MEGA-BUTTON */
+        // MEGA-DETECTOR MEGA-BUTTON
         Button detectBtn = (Button)findViewById(R.id.detectBtn);
         detectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,20 +36,19 @@ public class MainActivity extends AppCompatActivity {
 
                 Resources res = getResources();
 
-                /** getting strings with variables from strings.xml */
+                // getting strings with variables from strings.xml
                 String productEndString = String.format(res.getString(R.string.product), productSt);
                 String manufacturerEndSt = String.format(res.getString(R.string.manufacturer), manufacturerSt);
                 String deviceEndSt = String.format(res.getString(R.string.device), deviceSt);
                 String brandEndSt = String.format(res.getString(R.string.brand), brandSt);
                 String modelEndSt = String.format(res.getString(R.string.model), modelSt);
 
-                product.setText(productEndString); //LT30p
-                manufacturer.setText(manufacturerEndSt); // Sony
-                device.setText(deviceEndSt); /** mint; so far Build.DEVICE is best for detection */
-                brand.setText(brandEndSt); //Sony
-                model.setText(modelEndSt); //Xperia T
-
+                product.setText(productEndString); 
+                manufacturer.setText(manufacturerEndSt); 
+                device.setText(deviceEndSt); // so far Build.DEVICE is best for detection 
+                brand.setText(brandEndSt); 
+                model.setText(modelEndSt); 
             }
-        }); //end of onClickListener code
+        }); 
     }
 }
