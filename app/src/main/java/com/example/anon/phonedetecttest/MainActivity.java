2f10ba2348c94e3c32031d8522c3cfbf3b871d0e
dobrowins <dobrowins@gmail.com>
2017-01-21 12:09:31 +0300

    set onClickListener as separate method

--- app/src/main/java/com/example/anon/phonedetecttest/MainActivity.java
+++ app/src/main/java/com/example/anon/phonedetecttest/MainActivity.java
@@ -10,45 +10,48 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    // setting variables to work with
+    private String productSt = Build.PRODUCT;
+    private String manufacturerSt = Build.MANUFACTURER;
+    private String deviceSt = Build.DEVICE;
+    private String brandSt = Build.BRAND;
+    private String modelSt = Build.MODEL;
+    private TextView product;
+    private TextView manufacturer;
+    private TextView device;
+    private TextView brand;
+    private TextView model;
+
+    // TODO: 1/21/2017 add Butterknife - https://github.com/JakeWharton/butterknife 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // setting variables to work with
-        final String productSt = Build.PRODUCT;
-        final String manufacturerSt = Build.MANUFACTURER;
-        final String deviceSt = Build.DEVICE;
-        final String brandSt = Build.BRAND;
-        final String modelSt = Build.MODEL;
+        product = (TextView)findViewById(R.id.product);
+        manufacturer = (TextView)findViewById(R.id.manufacturer);
+        device = (TextView)findViewById(R.id.device);
+        brand = (TextView)findViewById(R.id.brand);
+        model = (TextView)findViewById(R.id.model);
+    }
 
-        final TextView product = (TextView)findViewById(R.id.product);
-        final TextView manufacturer = (TextView)findViewById(R.id.manufacturer);
-        final TextView device = (TextView)findViewById(R.id.device);
-        final TextView brand = (TextView)findViewById(R.id.brand);
-        final TextView model = (TextView)findViewById(R.id.model);
+    public void showPhoneInformation(View view) {
 
-        // MEGA-DETECTOR MEGA-BUTTON
-        Button detectBtn = (Button)findViewById(R.id.detectBtn);
-        detectBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        Resources res = getResources();
 
-                Resources res = getResources();
+        // getting strings with variables from strings.xml
+        String productEndString = String.format(res.getString(R.string.product), productSt);
+        String manufacturerEndSt = String.format(res.getString(R.string.manufacturer), manufacturerSt);
+        String deviceEndSt = String.format(res.getString(R.string.device), deviceSt);
+        String brandEndSt = String.format(res.getString(R.string.brand), brandSt);
+        String modelEndSt = String.format(res.getString(R.string.model), modelSt);
 
-                // getting strings with variables from strings.xml
-                String productEndString = String.format(res.getString(R.string.product), productSt);
-                String manufacturerEndSt = String.format(res.getString(R.string.manufacturer), manufacturerSt);
-                String deviceEndSt = String.format(res.getString(R.string.device), deviceSt);
-                String brandEndSt = String.format(res.getString(R.string.brand), brandSt);
-                String modelEndSt = String.format(res.getString(R.string.model), modelSt);
+        product.setText(productEndString);
+        manufacturer.setText(manufacturerEndSt);
+        device.setText(deviceEndSt);
+        brand.setText(brandEndSt);
+        model.setText(modelEndSt);
 
-                product.setText(productEndString); 
-                manufacturer.setText(manufacturerEndSt); 
-                device.setText(deviceEndSt); // so far Build.DEVICE is best for detection 
-                brand.setText(brandEndSt); 
-                model.setText(modelEndSt); 
-            }
-        }); 
     }
 }
